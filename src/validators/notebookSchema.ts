abe495a8379c19ae0170eea818a28efde142c2ee
pedrodevsz <pedrodevs25@gmail.com>
2025-08-09 11:52:31 -0300

    fix: modify category schema

--- src/validators/notebookSchema.ts
+++ src/validators/notebookSchema.ts
@@ -3,5 +3,5 @@ import z from "zod";
 export const notebookSchema = z.object({
   title: z.string().min(1, "Título é obrigatório"),
   description: z.string().optional(),
-  category: z.string().optional(),
+  category: z.string().min(1, "Categoria é obrigatória"),
 })
